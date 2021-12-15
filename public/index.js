367fd2aee9ef3da5ce73766111fef9873ec6249b
Seth <sethyuan@gmail.com>
2021-12-15 13:59:01 +0800

    fix: use another approach to insert margin

--- public/index.js
+++ public/index.js
@@ -1,33 +1,93 @@
 import "@logseq/libs"
 
-function addSpacing(str) {
-  const hanzi =
-    "[\u2E80-\u2FFF\u31C0-\u31EF\u3300-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\uFE30-\uFE4F]"
-  const punc = {
-    base: "[@&=_\\$%\\^\\*-\\+/]",
-    open: "[\\(\\[\\{‘“]",
-    close: "[,\\.\\?!:\\)\\]\\}’”]",
-  }
-  const latin =
-    "[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]" +
-    "|" +
-    punc.base
-  const patterns = [
-    new RegExp(
-      "(" + hanzi + ")((?:<[^>]*>)?" + latin + "|" + punc.open + ")",
-      "ig",
-    ),
-    new RegExp(
-      "((?:" + latin + "|" + punc.close + ")(?:<[^>]*>)?)(" + hanzi + ")",
-      "ig",
-    ),
-  ]
+const hanzi =
+  "[\u2E80-\u2FFF\u31C0-\u31EF\u3300-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\uFE30-\uFE4F]"
+const punc = {
+  base: "[@&=_\\$%\\^\\*-\\+/]",
+  open: "[\\(\\[\\{'\"]",
+  close: "[,\\.\\?!:\\)\\]\\}'\"]",
+}
+const latin =
+  "[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]" +
+  "|" +
+  punc.base
+const patterns = [
+  new RegExp("(" + hanzi + ")(" + latin + "|" + punc.open + ")", "ig"),
+  new RegExp("(" + latin + "|" + punc.close + ")(" + hanzi + ")", "ig"),
+]
 
-  for (const pattern of patterns) {
-    str = str.replace(pattern, `$1<span class="kef-char-spacing"></span>$2`)
+function addSpacing(el) {
+  const textNodes = Array.from(getTextNodes(el))
+  for (let i = 0; i < textNodes.length; i++) {
+    const textNode = textNodes[i]
+    // Join text with the first character of the next text node for
+    // pattern matching.
+    const text = `${textNode.data}${textNodes[i + 1]?.data[0] ?? ""}`
+
+    // Find spliting indices.
+    const indices = patterns
+      .map((p) => Array.from(text.matchAll(p)).map((m) => m.index + 1))
+      .reduce((ret, x) => ret.concat(x))
+      .sort((a, b) => a - b)
+
+    splitTextNode(textNode, indices, el)
+  }
+}
+
+function* getTextNodes(node) {
+  for (const subnode of node.childNodes) {
+    switch (subnode.nodeType) {
+      case 3:
+        yield subnode
+        break
+      case 1:
+        yield* getTextNodes(subnode)
+        break
+    }
+  }
+}
+
+function splitTextNode(node, indices, host) {
+  if (!indices?.length) return
+
+  const frag = document.createDocumentFragment()
+  let lastNode = node.cloneNode()
+  let offset = 0
+
+  for (const index of indices) {
+    const len = index - offset
+    offset = index
+
+    if (len < lastNode.data.length) {
+      frag.appendChild(document.createTextNode(lastNode.data.substring(0, len)))
+      const spacing = document.createElement("span")
+      spacing.classList.add("kef-char-spacing")
+      frag.appendChild(spacing)
+      lastNode = document.createTextNode(lastNode.data.substring(len))
+    }
+  }
+  frag.appendChild(lastNode)
+
+  // Calculate where this last spacing should go to.
+  if (
+    indices[indices.length - 1] >= node.data.length &&
+    node.parentElement != null
+  ) {
+    if (node.parentElement === host) {
+      const spacing = document.createElement("span")
+      spacing.classList.add("kef-char-spacing")
+      frag.appendChild(spacing)
+    } else {
+      let parent = node.parentElement
+      while (parent.parentElement != null && parent.parentElement !== host) {
+        if (parent !== parent.parentElement.lastChild) break
+        parent = parent.parentElement
+      }
+      parent.classList.add("kef-char-spacing")
+    }
   }
 
-  return str
+  node.parentNode.replaceChild(frag, node)
 }
 
 logseq
@@ -43,26 +103,18 @@ logseq
     const observer = new MutationObserver((mutationList) => {
       for (const mutation of mutationList) {
         for (const node of mutation.addedNodes) {
-          const inlineNodes = node.querySelectorAll("span.inline")
-          for (const inlineNode of inlineNodes) {
-            inlineNode.innerHTML = addSpacing(inlineNode.innerHTML)
+          if (node.querySelectorAll) {
+            const nodes = node.querySelectorAll("span.inline, td, th")
+            for (const n of nodes) {
+              addSpacing(n)
+            }
           }
         }
       }
     })
-    observer.observe(parent.document.querySelector("#app-container"), {
+    observer.observe(parent.document.body, {
       subtree: true,
       childList: true,
     })
-
-    // Initial processing.
-    setTimeout(() => {
-      const startupNodes = parent.document.querySelectorAll(
-        "#app-container .block-content span.inline",
-      )
-      for (const node of startupNodes) {
-        node.innerHTML = addSpacing(node.innerHTML)
-      }
-    }, 1000)
   })
   .catch(console.error)
